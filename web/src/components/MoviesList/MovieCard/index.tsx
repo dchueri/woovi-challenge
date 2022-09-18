986d465da23378c5d95ec9a57a7a090eeecd969a
diego <diegochueri@gmail.com>
2022-09-18 11:33:51 -0400

    feat(cards): added button for delete movies cards

--- web/src/components/MoviesList/MovieCard/index.tsx
+++ web/src/components/MoviesList/MovieCard/index.tsx
@@ -1,24 +1,58 @@
+import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
+import fetchGraphQL from "../../../fetchGraphQL";
 import { IMovie, IMovieEdge } from "../../../types/MovieTypes";
 
-const card = (movie: IMovie) => (
-  <React.Fragment>
-    <CardContent>
-      <Typography variant="h5" component="div">
-        {movie.title}
-      </Typography>
-      <Typography color="text.secondary">
-        {movie.genre}
-      </Typography>
-    </CardContent>
-  </React.Fragment>
-);
+export default function MovieCard(props: {
+  movie: IMovieEdge;
+  moviesList: IMovieEdge[];
+  setMoviesList: React.Dispatch<React.SetStateAction<IMovieEdge[]>>;
+}) {
+  const handleDeleteMovieOfList = (id: string) => {
+    props.setMoviesList((prev) => prev.filter((movie) => movie.node.id !== id));
+  };
 
-export default function MovieCard(props: { movie: IMovieEdge }) {
+  const handleDeleteMovie = async (id: string) => {
+    const variables = { id: id };
+    fetchGraphQL(
+      `
+      mutation Movies($id: String!){
+        DeleteMovie(input: {id: $id}) {
+          error
+          success
+        }
+      }
+          `,
+      variables
+    )
+      .then((response) => {
+        handleDeleteMovieOfList(id);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const card = (movie: IMovie) => (
+    <React.Fragment>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {movie.title}
+        </Typography>
+        <Typography color="text.secondary">{movie.genre}</Typography>
+        <DeleteForeverOutlinedIcon
+          onClick={() => {
+            handleDeleteMovie(movie.id);
+          }}
+        />
+      </CardContent>
+    </React.Fragment>
+  );
+
   return (
     <Box sx={{ width: "fit-content", padding: "1em", display: "inline-flex" }}>
       <Card variant="outlined">{card(props.movie.node)}</Card>
