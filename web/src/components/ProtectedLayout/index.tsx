212142a568f08a5d8f1a7180f213bc040558ef31
diegochueri <diegochueri@gmail.com>
2022-09-26 12:54:15 -0400

    fix(protected-layout): fixed the flash print of ProtectedLayout element on screen

--- web/src/components/ProtectedLayout/index.tsx
+++ web/src/components/ProtectedLayout/index.tsx
@@ -6,11 +6,27 @@ export const ProtectedLayout = ({ children }: { children: JSX.Element }) => {
   const StyledH1 = styled.h1`
     margin: auto;
   `;
+
+  const Content = (): any => {
+    let result;
+    setTimeout(() => {
+      result = <StyledH1>You don't have access. Sign in!</StyledH1>;
+    }, 500);
+    return result;
+  };
+
   const auth = useAuth();
   if (!auth.me) {
     return (
-      <Container sx={{ height: "95vh", margin: "0 auto", textAlign: "center" }}>
-        <StyledH1>You don't have access. Sign in!</StyledH1>
+      <Container
+        sx={{
+          height: "90vh",
+          margin: "0 auto",
+          textAlign: "center",
+          display: "flex",
+        }}
+      >
+        <Content />
       </Container>
     );
   }
