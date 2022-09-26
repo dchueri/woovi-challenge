df5ff75a71361d5df33e6eb6b000aa2b1b0cd6c0
diegochueri <diegochueri@gmail.com>
2022-09-26 18:45:09 -0400

    feat(logout): added logout button

--- web/src/components/MainPanel/index.tsx
+++ web/src/components/MainPanel/index.tsx
@@ -1,6 +1,8 @@
-import { CircularProgress } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { Button, CircularProgress } from "@mui/material";
 import { Suspense } from "react";
 import styled from "styled-components";
+import { useAuth } from "../../context/AuthProvider/useAuth";
 import MovieForm from "./MovieForm";
 import MoviesList from "./MoviesList";
 
@@ -12,7 +14,22 @@ const Div = styled.div`
   align-items: center;
 `;
 
+const LogoutDiv = styled.div`
+  width: 100vw;
+  height: 15vh;
+  position: fixed;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
 function MainPanel() {
+  const auth = useAuth();
+
+  const logoutHandle = () => {
+    auth.logout();
+  };
+
   return (
     <Suspense
       fallback={
@@ -21,6 +38,22 @@ function MainPanel() {
         </Div>
       }
     >
+      <LogoutDiv>
+        <Button
+          onClick={logoutHandle}
+          variant="text"
+          size="small"
+          sx={{
+            margin: "2vw",
+            borderRadius: "50px",
+            height: "fit-content",
+            minWidth: "fit-content",
+            padding: "1em",
+          }}
+        >
+          <LogoutIcon />
+        </Button>
+      </LogoutDiv>
       <MovieForm />
       <MoviesList />
     </Suspense>
