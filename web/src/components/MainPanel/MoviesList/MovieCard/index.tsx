b715ff7c98517d63b5fdfc2ef055fccd078ce6dc
diegochueri <diegochueri@gmail.com>
2022-09-28 18:37:05 -0400

    feat(movies-detail): added a expansive page for movies details

--- web/src/components/MainPanel/MoviesList/MovieCard/index.tsx
+++ web/src/components/MainPanel/MoviesList/MovieCard/index.tsx
@@ -1,20 +1,34 @@
 import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
+import { Button } from "@mui/material";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { ConnectionHandler, useMutation } from "react-relay";
 import { ROOT_ID } from "relay-runtime";
 import styled from "styled-components";
 import { DeleteMovieMutation } from "../../../../modules/movie/DeleteMovieMutation";
 import { IMovie, IMovieEdge } from "../../../../types/MovieTypes";
+import MovieDetails from "../../../MovieDetails";
+import Details from "../../../MovieDetails/Details";
 
 const Img = styled.img`
-  max-width: 10em;
+  width: 10em;
 `;
 
-export default function MovieCard(props: { movie: IMovieEdge | null }) {
+export default function MovieCard(props: { movie: IMovieEdge }) {
   const [deleteMovieMutation] = useMutation(DeleteMovieMutation);
+  const [open, setOpen] = useState(false);
+
+  const openWindow = () => {
+    setOpen(true);
+  };
+
+  const closeWindow = () => {
+    setOpen(false);
+  };
 
   const connectionID = ConnectionHandler.getConnectionID(ROOT_ID, "All_movies");
 
@@ -27,27 +41,31 @@ export default function MovieCard(props: { movie: IMovieEdge | null }) {
   };
 
   const card = (movie: IMovie) => (
-    <CardContent
-      sx={{
-        textAlign: "center",
-        gap: "0.5em",
-        display: "grid",
-        justifyItems: "center",
-        padding: "1em",
-      }}
-    >
-      <Img src={movie.image} />
-      <Typography variant="h5" component="div">
-        {movie.title}
-      </Typography>
-      <Typography color="text.secondary">{movie.genre}</Typography>
-      <DeleteForeverOutlinedIcon
-        color="error"
-        onClick={() => {
-          handleDeleteMovie(movie.id);
+    <motion.div className="listing" onClick={openWindow}>
+      <CardContent
+        sx={{
+          textAlign: "center",
+          gap: "0.5em",
+          display: "grid",
+          justifyItems: "center",
+          padding: "1em",
         }}
-      />
-    </CardContent>
+      >
+        <Img src={movie.image} />
+        <Typography variant="h5" component="div">
+          {movie.title}
+        </Typography>
+        <Typography color="text.secondary">{movie.genre}</Typography>
+        <Button color="error" sx={{ minWidth: "auto", borderRadius: "50px" }}>
+          <DeleteForeverOutlinedIcon
+            color="action"
+            onClick={() => {
+              handleDeleteMovie(movie.id);
+            }}
+          />
+        </Button>
+      </CardContent>
+    </motion.div>
   );
 
   return (
@@ -58,9 +76,34 @@ export default function MovieCard(props: { movie: IMovieEdge | null }) {
         display: "inline-flex",
       }}
     >
-      <Card variant="outlined" sx={{ borderRadius: "20px", minWidth: "7em" }}>
-        {card(props.movie!.node)}
-      </Card>
+      <AnimatePresence mode={"sync"}>
+        <motion.div
+          layout
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          exit={{ scale: 0.8, opacity: 0 }}
+          transition={{ type: "spring", damping: 20, stiffness: 100 }}
+          whileTap={{ scale: 0.8 }}
+          whileHover={{ scale: 1.1 }}
+        >
+          <Card
+            variant="outlined"
+            sx={{ borderRadius: "20px", minWidth: "7em" }}
+          >
+            {card(props.movie!.node)}
+          </Card>
+        </motion.div>
+      </AnimatePresence>
+      <AnimatePresence>
+        {open && (
+          <MovieDetails
+            close={closeWindow}
+            children={
+              <Details movie={props.movie!.node} closeWindow={closeWindow} />
+            }
+          ></MovieDetails>
+        )}
+      </AnimatePresence>
     </Box>
   );
 }
