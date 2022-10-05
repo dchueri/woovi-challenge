b1d19dabccde029287cc70bba15ced02d9ed3588
diegochueri <diegochueri@gmail.com>
2022-10-05 12:19:00 -0400

    feat(movies-form): now the movies recived a genre automatically

--- packages/web/src/utils/moviesDB.ts
+++ packages/web/src/utils/moviesDB.ts
@@ -1,9 +1,16 @@
 import axios from "axios";
+import genreList from "./GenreLists.json";
 
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/search/",
   timeout: 1000,
 });
+
+type GenresList = keyof typeof genreList;
+
+function setGenre(genreId: GenresList): string {
+  return genreList[genreId] ? genreList[genreId] : "Off genre";
+}
 
 export async function getMovieInfos(title: string) {
   const res = await api.get(
@@ -11,9 +18,11 @@ export async function getMovieInfos(title: string) {
   );
 
   const image = `https://image.tmdb.org/t/p/original/${res.data.results[0].poster_path}`;
+  const genre = setGenre(res.data.results[0].genre_ids[0]);
+  console.log(genre);
   const description = res.data.results[0].overview;
   const averageString = (res.data.results[0].vote_average / 2).toFixed(1);
   const average = parseFloat(averageString);
-  const moviesInfos = { image, description, average };
+  const moviesInfos = { image, description, average, genre };
   return moviesInfos;
 }
