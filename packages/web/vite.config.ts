565ff1c5075c703fa7e9ec3ce4489f3f9ab69a1a
diegochueri <diegochueri@gmail.com>
2022-10-07 14:52:39 -0400

    refactor: refactored the web for recive tests

---
+++ packages/web/vite.config.ts
@@ -0,0 +1,11 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import relay from "vite-plugin-relay";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), relay],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+});
